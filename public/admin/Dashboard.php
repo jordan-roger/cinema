b67f79f1b15c74b421fdb138da1400ef9158b660
rdjamga1919 <rdjamga@gmail.com>
2026-04-12 13:30:12 +0200

    page admin

--- public/admin/Dashboard.php
+++ public/admin/Dashboard.php
@@ -1,12 +1,1 @@
 <?php
-function estAdmin(): bool {
-    return isset($_SESSION['role']) && $_SESSION['role'] === 'admin';
-}
-function exigerAdmin(): void
-{
-    if (!estAdmin()) {
-        $_SESSION['erreurs'] = ["Accès refusé."];
-        header('Location: ../connexion.php');
-        exit;
-    }
-}
