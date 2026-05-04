ed91b8371c308ff5a455095bd8baacf9f81472f9
kibeti <s.kibeti@lprs.fr>
2026-05-04 21:31:26 +0200

    creation et aplication de la navbar sur toute les pages "client"

--- public/reservationClient.php
+++ public/reservationClient.php
@@ -1,1 +1,23 @@
 <?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>MK2 CINÉ</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<nav>
+    <a href="index.php" class="nav-logo">MK<span>2</span> CINÉ</a>
+
+    <div class="nav-links">
+        <a href="profil.php">Voir mon profil</a>
+        <a href="mes_reservations.php">Mes réservations</a>
+        <a href="logout.php">Déconnexion</a>
+    </div>
+</nav>
